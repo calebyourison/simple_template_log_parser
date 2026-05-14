ec7f5a61afda9df8215f557b6253537e8713eeba
caleb <caleb.yourison@gmail.com>
2026-05-14 12:53:10 -0700

    omv template updates

--- template_workflows/templates/omv.py
+++ template_workflows/templates/omv.py
@@ -4,6 +4,7 @@ openmediavault_id_process = "{time} {server_name} openmediavault-{process}[{id}]
 openmediavault_process = "{time} {server_name} openmediavault-{process} {message}"
 omv_id_process = "{time} {server_name} omv-{process}[{id}]:{message}"
 omv_process = "{time} {server_name} omv-{process}: {message}"
+omv_id_process_2 = "{time} {server_name} omv-{process}[{id}]:"
 conf = "{time} {server_name} conf_{version}: {message}"
 php = "{time} {server_name} php{version}: {action}: {message}"
 
@@ -11,6 +12,7 @@ php = "{time} {server_name} php{version}: {action}: {message}"
 omv_process_templates = [
     [omv_id_process, "omv_id_process", " omv-"],
     [omv_process, "omv_process", "omv-"],
+    [omv_id_process_2, "omv_id_process", "omv-"]
 ]
 
 openmediavault_process_templates = [
