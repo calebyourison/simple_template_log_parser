7feefd9f319336a52aba923195343a30bf39acd5
caleb <caleb.yourison@gmail.com>
2026-05-23 05:11:52 -0700

    kodi template updates

--- template_workflows/templates/kodi.py
+++ template_workflows/templates/kodi.py
@@ -1,12 +1,16 @@
 # System info
 device = "{time} T:{T} {level} <{category}>:     Device {device_number} m_deviceName :{name}m_displayName :{display_name}m_displayNameExtra:{display_name_extra}m_deviceType :{type}m_channels :{channels}m_sampleRates :{sample_rates}m_dataFormats :{data_formats}m_streamTypes :{stream_types}"
+external_storage = "{time} T:{T} {level} <{category}>: External storage path = {path}; status = {status}; Permissions = {permissions}"
 file_contents = "{time} T:{T} {level} <{category}>: Contents of {file} are... {contents}"
 ffmpeg_version = "{time} T:{T} {level} <{category}>: FFmpeg version/source: {version}"
 host_cpu = "{time} T:{T} {level} <{category}>: Host CPU: {cpu}, {cores} cores available"
 kodi_compiled = "{time} T:{T} {level} <{category}>: Kodi compiled {compiled_date} by {compiler} for {platform}"
+local_hostname = "{time} T:{T} {level} <{category}>: Local hostname: {hostname}"
+log_file_location = "{time} T:{T} {level} <{category}>: Log File is located: {path}"
 mapped_directory = "{time} T:{T} {level} <{category}>: {kodi_directory} is mapped to: {local_directory}"
 product = "{time} T:{T} {level} <{category}>: Product: {product}, Device: {device} Board: {board} Manufacturer: {manufacturer} Brand: {brand} Model: {model} Hardware: {hardware}"
 starting_kodi = "{time} T:{T} {level} <{category}>: Starting Kodi ({version}). Platform: {platform}"
+userdata_folder = "{time} T:{T} {level} <{category}>: userdata folder: {path}"
 using_release = "{time} T:{T} {level} <{category}>: Using Release {release}"
 new_cache_gui_settings = "{time} T:{T} {level} <{category}>: New Cache GUI Settings (replacement of cache in {replaced_cache}) are: Buffer Mode: {buffer_mode} Memory Size: {memory_size} Read Factor: {read_factor} x Chunk Size : {chunk_size}"
 
@@ -19,16 +23,19 @@ program_action_2 = "{time} T:{T} {level} <{category}>: {program}: {action}"
 # General
 closing = "{time} T:{T} {level} <{category}>: Closing {info}"
 creating = "{time} T:{T} {level} <{category}>: Creating {info}"
+creating_2 = "{time} T:{T} {level} <{category}>: creating {info}"
 deleting = "{time} T:{T} {level} <{category}>: Deleting {info}"
 deleting_2 = "{time} T:{T} {level} <{category}>: deleting {info}"
 finding = "{time} T:{T} {level} <{category}>: Finding {info}"
-instancing = "{time} T:{T} {level} <{category}>: Instancing {instance}"
+instancing = "{time} T:{T} {level} <{category}>: Instancing {info}"
 opening = "{time} T:{T} {level} <{category}>: Opening {info}"
 running = "{time} T:{T} {level} <{category}>: Running {info}"
 saving = "{time} T:{T} {level} <{category}>: Saving {info}"
+starting = "{time} T:{T} {level} <{category}>: starting {info}"
 stopping = "{time} T:{T} {level} <{category}>: Stopping {info}"
 graphics_library = "{time} T:{T} {level} <{category}>: {graphics_type}_{feature} = {info}"
 load = "{time} T:{T} {level} <{category}>:   load {info}"
+load_2 = "{time} T:{T} {level} <{category}>: load {info}"
 loaded = "{time} T:{T} {level} <{category}>: Loaded {info}"
 loading = "{time} T:{T} {level} <{category}>: Loading {config}"
 loading_2 = "{time} T:{T} {level} <{category}>: loading {config}"
@@ -43,6 +50,7 @@ audio_buffer_params = "{time} T:{T} {level} <{category}>: Audiotrack buffer para
 minimum_stream_size = "{time} T:{T} {level} <{category}>: Minimum size we need for stream: {bytes} Bytes"
 trying_to_open = "{time} T:{T} {level} <{category}>: Trying to open: samplerate: {samplerate}, channelMask: {channel_mask}, encoding: {encoding}"
 register_device = "{time} T:{T} {level} <{category}>: Register - new {device} device registered on {application}->{info}"
+is_supported = "{time} T:{T} {level} <{category}>: {info} is supported"
 
 
 debug_message = "{time} T:{T} debug <{category}>: {message}"
@@ -74,7 +82,10 @@ program_action_search_names = [
     "VideoPlayer",
     "JSONRPC",
     "CAndroid",
-    "CLangInfo"
+    "CLangInfo",
+    "CMedia",
+    "CPeripheral",
+    "Load"
 ]
 
 for name in program_action_search_names:
@@ -83,17 +94,20 @@ for name in program_action_search_names:
 general_templates = [
     [closing, "closing", "Closing"],
     [creating, "creating", "Creating"],
+    [creating_2, "creating", "creating"],
     [deleting, "deleting", "Deleting"],
     [deleting_2, "deleting", "deleting"],
     [finding, "finding", "Finding"],
     [opening, "opening", "Opening"],
     [running, "running", "Running"],
+    [starting, "starting", "starting"],
     [stopping, "stopping", "Stopping"],
     [mediacodec_decoder, "mediacodec_decoder", "Mediacodec decoder"],
     [graphics_library, "graphics_library", "GL_"],
     [loading, "loading", "Loading"],
     [loading_2, "loading", "loading"],
     [load, "load", "load"],
+    [load_2, "load", "load"],
     [loaded, "loaded", "Loaded"],
     [update, "update", "Update"],
     [remote_mapping, "remote_mapping", "remote mapping"],
@@ -107,20 +121,25 @@ general_templates = [
     [instancing, "instancing", "Instancing"],
     [trying_to_open, "trying_to_open", "Trying to open"],
     [register_device, "register_device", "Register"],
+    [is_supported, "supported", "is supported"],
 ]
 
 
 system_info_templates = [
     [device, "device", "Device"],
+    [external_storage, "external_storage", "External storage"],
     [file_contents, "file_contents", "Contents"],
     [ffmpeg_version, "ffmpeg_version", "FFmpeg"],
     [host_cpu, "host_cpu", "Host CPU"],
     [kodi_compiled, "kodi_compiled", "Kodi compiled"],
+    [local_hostname, "local_hostname", "Local hostname"],
+    [log_file_location, "log_file_location", "Log File is located"],
     [mapped_directory, "mapped_directory", "is mapped to"],
     [product, "product", "Product"],
+    [userdata_folder, "userdata_folder", "userdata folder"],
     [using_release, "using_release", "Using Release"],
     [starting_kodi, "starting_kodi", "Starting Kodi"],
-    [new_cache_gui_settings, "new_cache_gui_settings", "New Cache GUI Settings"]
+    [new_cache_gui_settings, "new_cache_gui_settings", "New Cache GUI Settings"],
 ]
 
 base_kodi_templates = debug_templates + warning_templates + error_templates + program_action_templates  + general_templates + system_info_templates
